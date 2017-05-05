9c36467ee1b62813bbac6cc124889fac3d80a477
elivlo <eliasfloetzinger@gmx.de>
2017-05-05 13:22:30 +0200

    Update FishServer.java

--- FishServer.java
+++ FishServer.java
@@ -112,7 +112,7 @@ public class FishServer {
             		fishSaisonStart = true;
 
             		try {
-						Thread.sleep(5000);
+						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
             		}
