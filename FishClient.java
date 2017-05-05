07bd3fd6968704e794002e579e29135ef1010497
elivlo <eliasfloetzinger@gmx.de>
2017-05-05 16:06:08 +0200

    Update FishClient.java

--- FishClient.java
+++ FishClient.java
@@ -1,26 +1,109 @@
-package main;
+package fishClientconsole;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.io.*;
+import java.util.Objects;
+import java.util.Scanner;
 
-/**
- * Created by Joachim on 02.04.2017.
- */
+
+
+
+
 public class FishClient {
+  
+  static boolean akzeptanz = false;
+  static Socket socket;
+  static Scanner inputsc;
+  
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1",10101);
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
+                                                                               
+    try {
+      socket = new Socket("127.0.0.1",8080);
+        
+      BufferedReader inputsc = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket.getOutputStream()); 
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+      String nickname;
+      
+      System.out.print("Nickname: ");
+      nickname = inputsc.readLine() + "\n";
+      out.write(nickname);
+      out.flush();  
+      
+      System.out.println("FISH, BUY, SELL, STATS, leave");
+      System.out.println("----------------------------");
+      
+      
+      new Thread(new serverInput()).start();
+      new Thread(new serverOutput()).start();
+      
+      
+      
 
-        out.write("FISH 5");
-        in.readLine();
-
-        out.write("SELL 20");
-        double amount = Double.parseDouble(in.readLine());
-        System.out.println("Got "+ amount + "€!!!");
+      
+    } catch(Exception e) {
+      e.printStackTrace();
     }
+    
+       
+        
+        
+    }
+    
+    static class serverOutput implements Runnable{
+        public void run() {
+          String[] input;
+            String toserver;
+          try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket.getOutputStream()); 
+        while (true) {
+              
+              toserver = in.readLine();
+              
+              if (toserver=="leave"){
+                  out.close();
+                  System.exit(0);
+              } else{
+                out.write(toserver+"\n");
+                out.flush();
+              }
+
+              
+            }
+        
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+        }
+    }
+    
+    static class serverInput implements Runnable{
+        public void run() {
+          try {
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String output;
+        
+        while(true){
+          output = in.readLine();
+          System.out.println(output);
+          
+          
+        }
+        
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+        }
+    }
+   
 }
+
+
+
+
